b6ccb4e764f8586c379d2b177c5258d4d6b15344
Tim Loden <tim.m.loden@gmail.com>
2020-07-23 11:01:29 -0700

    fixed data sheet links

--- page-data-sheets.php
+++ page-data-sheets.php
@@ -12,7 +12,7 @@ get_header();
 <section class="bg-white py-lg-5">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-12 col-lg-8">
+            <div class="col-12 col-lg-10">
                 <h1 class="text-center mb-4">Safety Data Sheets</h1>
                 <p class="text-center">If you have any questions, please call us at
                     <strong><?php echo get_field('phone_number', 'option'); ?></strong></p>
@@ -21,8 +21,8 @@ get_header();
                     <thead>
                         <tr>
                             <th scope="col">Product</th>
-                            <th scope="col">Safety Data Sheet</th>
-                            <th scope="col">Label</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
                         </tr>
                     </thead>
                     <?php while( have_rows('data_sheets') ): the_row(); 
@@ -31,8 +31,8 @@ get_header();
                     ?>
                     <tr>
                         <td><?php the_sub_field('name'); ?></td>
-                        <td><a href="<?php echo $sheet['url']; ?>" target="_blank">Safety Data Sheet</a></td>
-                        <td><a href="<?php echo $label['url']; ?>" target="_blank">Label</a></td>
+                        <td><a href="<?php echo $sheet; ?>" target="_blank">Safety Data Sheet</a></td>
+                        <td><a href="<?php echo $label; ?>" target="_blank">Label</a></td>
                     </tr>
                     <?php endwhile; ?>
                 </table>
