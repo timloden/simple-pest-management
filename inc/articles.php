028942a595a3b7ed1b11e34f2a4d36dc46e5aae7
Tim Loden <tim.m.loden@gmail.com>
2020-09-23 16:20:13 -0700

    updated posts per page for pests and service areas

--- inc/articles.php
+++ inc/articles.php
@@ -6,3 +6,11 @@ function wpdocs_custom_excerpt_length( $length ) {
     return 20;
 }
 add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+function custom_type_archive_display($query) {
+    if (is_post_type_archive('service-areas') || is_post_type_archive('pests')) {
+         $query->set('posts_per_page',-1);
+        return;
+    }     
+}
+add_action('pre_get_posts', 'custom_type_archive_display');
