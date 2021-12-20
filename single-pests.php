59b779ea8a06d79d7376fc3e741f499b80a87e06
Tim Loden <tim.m.loden@gmail.com>
2021-12-20 09:30:01 -0800

    added link to pest detail

---
+++ single-pests.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package underscores
+ */
+
+get_header();
+$featured_img = get_the_post_thumbnail_url();
+$hero_title = get_field('hero_title');
+?>
+<div id="primary" class="content-area services-single">
+    <main id="main" class="site-main">
+
+        <section class="service-hero">
+            <div class="jumbotron jumbotron-fluid bg-white mb-0">
+
+                <div class="container py-lg-5">
+                    <div class="row">
+                        <div class="col-12 mb-5 mb-lg-0">
+                            <?php
+                            while ( have_posts() ) :
+                                the_post();
+                                if ($hero_title) {
+                                    echo '<h1 class="mb-4 text-center">' . $hero_title . '</h1>';
+                                } else {
+                                    echo '<h1 class="mb-4 text-center">' . get_the_title() . '</h1>';
+                                }
+                                
+                                get_template_part( 'template-parts/content-single', get_post_type() );
+
+                            endwhile; // End of the loop.
+                            ?>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+
+
+
+
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
--- template-parts/content-pests.php
+++ template-parts/content-pests.php
@@ -8,14 +8,14 @@
  */
 ?>
 <div class="col mb-3">
-
-    <div class="card h-100 post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <?php the_post_thumbnail('medium', ['class' => 'card-img-top lazy', 'title' => 'Feature image']);?>
-        <div class="card-body">
-            <h5 class="card-title"><?php the_title(); ?></h5>
-            <p class="card-text"><?php the_excerpt(); ?></p>
+    <a class="text-dark text-decoration-none" href="<?php the_permalink(); ?>">
+        <div class="card h-100 post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php the_post_thumbnail('medium', ['class' => 'card-img-top lazy', 'title' => 'Feature image']);?>
+            <div class="card-body">
+                <h5 class="card-title text-dark"><?php the_title(); ?></h5>
+                <p class="card-text text-dark"><?php the_excerpt(); ?></p>
+            </div>
         </div>
-    </div>
-
+    </a>
 
 </div>
