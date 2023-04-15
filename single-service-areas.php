7c19192448bbc3377dfa58593a1e0514bddc087a
Tim Loden <tim.m.loden@gmail.com>
2023-04-14 21:14:49 -0700

    updated service areas section

--- single-service-areas.php
+++ single-service-areas.php
@@ -13,63 +13,241 @@ $city = get_field('city');
 <div id="primary" class="content-area services-single">
     <main id="main" class="site-main">
 
-        <section class="service-hero">
-
-
-            <div class="container py-5">
+        <section class="service-hero"
+            style="background-image: url(https://www.simplepestmanagement.local/wp-content/uploads/2022/07/simple-pest-management-dangers-of-diy-pest-control.jpg)">
+            <div class="container">
                 <div class="row">
-                    <div class="col-12 mb-5 mb-lg-0">
-                        <?php
+                    <div class="col-12 col-lg-6">
+                        <h1 class="text-white">Your local and best pest control service in <?php echo $city; ?>,
+                            California</h1>
+                        <p class="text-white">Simple Pest Management provides the <?php echo $city; ?> area with
+                            reliable & effective
+                            elimination of insects and rodents. We have pest control programs for all different types or
+                            residences including single family homes, condos, and apartment buildings. </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-10">
+                    <p class="mb-0">We also service commercial properties including food processing facilities,
+                        restaurants, schools, office buildings and many other types of locations. At Simple Pest
+                        Management, we go out of our way to tailor service to our customer&apos;s specific needs;
+                        whether they are looking for year-round protection or a one-time service.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <p class="kicker">Latest Technology</p>
+                    <h2>Simple Pest Management strives to exceed our client&apos;s expectations</h2>
+                    <p>from the moment our technicians arrive. All of our technicians are uniformed are licensed, bonded
+                        and insured. But beyond these basics, you will find that they are professionals and will treat
+                        your home as they would that if their own family. We use the latest technology available in the
+                        pest control industry and have the experience to understand how and when to use the advanced
+                        tools and when to use basic, old fashioned hard work.</p>
+                </div>
+
+                <div class="col-12 col-lg-6 service-area-image-bg right">
+                    <img class="img-fluid"
+                        src="https://www.simplepestmanagement.local/wp-content/uploads/2022/07/simple-pest-management-dangers-of-diy-pest-control.jpg">
+                </div>
+            </div>
+        </div>
+
+        <section class="service-area-cta bg-secondary">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-lg-8 text-center text-lg-start">
+                        <p class="h2">100% Satisfaction Guaranteed</p>
+                        <p class="mb-0">This is the difference with our company. We understand that technology
+                            won&apos;t always solve the problem if you don&apos;t have skilled technicians in control.
+                        </p>
+                    </div>
+                    <div class="col-12 col-lg-4 text-center text-lg-end">
+                        <a class="btn btn-primary" href="tel:">(833) 887-7378 (PEST)</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6 service-area-image-bg left">
+                    <img class="img-fluid"
+                        src="https://www.simplepestmanagement.local/wp-content/uploads/2022/07/simple-pest-management-dangers-of-diy-pest-control.jpg">
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <p class="kicker">Reliable &amp; Effective Elimination</p>
+                    <h2>Simple Pest Management provides the <?php echo $city; ?> are with reliable</h2>
+                    <p>effective elimination of pests, bugs, insects and rodents. We have pest control programs for all
+                        different types of residences including single family homes, condos, and apartment buildings.
+                        We also service commercial properties including food processing facilities, restaurants,
+                        schools, office buildings and many other types of locations. At Simple Pest Management, we go
+                        out of our way to tailor service to our customer's specific needs, whether you are looking for
+                        year-round protection or a one-time service.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <p class="kicker">Experience to Understand</p>
+                    <h2>Simple Pest Management strives to exceed our client&apos;s expectations</h2>
+                    <p>from the moment our technicians arrive. All our technicians are uniformed, professional and
+                        licensed with the state. The company is licensed, bonded, and insured.</p>
+                    <p>But beyond these basics, you will find that they are professionals and will treat your home as
+                        they would that if their own family.
+                        We use the latest technology available in the pest control industry and have the experience to
+                        understand how and when to use the advanced tools and when to use basic, old fashioned hard
+                        work.</p>
+                </div>
+
+                <div class="col-12 col-lg-6 service-area-image-bg right">
+                    <img class="img-fluid"
+                        src="https://www.simplepestmanagement.local/wp-content/uploads/2022/07/simple-pest-management-dangers-of-diy-pest-control.jpg">
+                </div>
+            </div>
+        </div>
+
+        <section class="service-area-cta bg-secondary">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-lg-8 text-center text-lg-start">
+                        <p class="h2">Do you need pest control in <?php echo $city; ?>?</p>
+                        <p class="mb-0">Our team of pest experts is ready to take your call. We offer advice on pest
+                            control and a range of services to eliminate pests from your property. Don&apos;t let the
+                            pests gain control of your property. Call Simple Pest Management
+                        </p>
+                    </div>
+                    <div class="col-12 col-lg-4 text-center text-lg-end">
+                        <a class="btn btn-primary" href="tel:">(833) 887-7378 (PEST)</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6 service-area-image-bg left">
+                    <img class="img-fluid"
+                        src="https://www.simplepestmanagement.local/wp-content/uploads/2022/07/simple-pest-management-dangers-of-diy-pest-control.jpg">
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <h2>Residential Pest Control <?php echo $city; ?></h2>
+                    <p>If you own a residence in Santee, call Simple Pest Management to deal with your pest problem. Our
+                        team works with property managers, landlords, homeowners, and tenants. We offer pest inspections
+                        and estimates for any of the following properties.</p>
+                    <ul class="mb-0">
+                        <li>Apartment Pest Control in Santee.</li>
+                        <li>Duplex Pest Control in Santee.</li>
+                        <li>Single Family Home Pest Control in Santee.</li>
+                        <li>Condo Pest Control in Santee.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <p class="kicker">Experience to Understand</p>
+                    <h2>Commercial Pest Control <?php echo $city; ?></h2>
+                    <p>If you own a business or commercial building, call simple Pest Management for effective pest
+                        control services in Santee. We work with building managers, business owners, and employees.
+                        Contact our pest control team for immediate assistance.</p>
+                    <ul class="mb-0">
+                        <li>Pest Control Santee for Offices.</li>
+                        <li>Pest Control Santee for Retail.</li>
+                        <li>Pest Control Santee for Medical Centers.</li>
+                        <li>Pest Control Santee for Restaurants.</li>
+                        <li>Pest Control Santee for Schools.</li>
+                        <li>Pest Control Santee for Warehouses.</li>
+                        <li></li>Pest Control Santee for Supermarkets.</li>
+                    </ul>
+                </div>
+
+                <div class="col-12 col-lg-6 service-area-image-bg right">
+                    <img class="img-fluid"
+                        src="https://www.simplepestmanagement.local/wp-content/uploads/2022/07/simple-pest-management-dangers-of-diy-pest-control.jpg">
+                </div>
+            </div>
+        </div>
+
+        <section class="range-of-services">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h2>Our Range of <?php echo $city; ?> Pest Control Services</h2>
+                        <p>If you have a pest problem in Santee, contact Simple Pest Management at (619) 373-7378. We
+                            have the team, skills, and products to eliminate pests from your property. We have decades
+                            of experience working with homeowners and businesses in San Diego. We're confident we have
+                            the right pest control strategy for your property. Call our pest control team for a free
+                            inspection today.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-12 mb-5 mb-lg-0">
+                    <?php
                             while ( have_posts() ) :
                                 the_post();
                                ?>
 
-                        <div class="service-area-content">
+                    <div class="service-area-content">
 
-                            <h1 class="mb-5 text-center">Your local and best pest control service in
-                                <br><?php echo $city; ?>, California</h1>
+                        <h1 class="mb-5 text-center">Your local and best pest control service in
+                            <br><?php echo $city; ?>, California
+                        </h1>
 
-                            <p>Simple Pest Management provides the <?php echo $city; ?> area with reliable & effective
-                                elimination of insects
-                                and rodents. We
-                                have pest control programs for all different types of residences including single family
-                                homes, condos, and
-                                apartment buildings. We also service commercial properties including food processing
-                                facilities,
-                                restaurants, schools, office buildings and many other types of locations. At Simple Pest
-                                Management, we go
-                                out of our way to tailor service to our customer’s specific needs; whether they are
-                                looking for year-round
-                                protection or a one-time service.</p>
+                        <p>Simple Pest Management provides the <?php echo $city; ?> area with reliable & effective
+                            elimination of insects
+                            and rodents. We
+                            have pest control programs for all different types of residences including single family
+                            homes, condos, and
+                            apartment buildings. We also service commercial properties including food processing
+                            facilities,
+                            restaurants, schools, office buildings and many other types of locations. At Simple Pest
+                            Management, we go
+                            out of our way to tailor service to our customer’s specific needs; whether they are
+                            looking for year-round
+                            protection or a one-time service.</p>
 
-                            <p>Simple Pest Management strives to exceed our client’s expectations, from the moment our
-                                technicians arrive.
-                                All of our technicians are uniformed; are licensed, bonded and insured. But beyond these
-                                basics, you will
-                                find that they are professionals and will treat your home as they would that if their
-                                own family. We use the
-                                latest technology available in the pest control industry and have the experience to
-                                understand how and when
-                                to use the advanced tools and when to use basic, old fashioned hard work.</p>
+                        <p>Simple Pest Management strives to exceed our client’s expectations, from the moment our
+                            technicians arrive.
+                            All of our technicians are uniformed; are licensed, bonded and insured. But beyond these
+                            basics, you will
+                            find that they are professionals and will treat your home as they would that if their
+                            own family. We use the
+                            latest technology available in the pest control industry and have the experience to
+                            understand how and when
+                            to use the advanced tools and when to use basic, old fashioned hard work.</p>
 
-                            <p>This is the difference with our company. We understand that technology won’t always solve
-                                the problem if you
-                                don’t have skilled technicians in control.</p>
+                        <p>This is the difference with our company. We understand that technology won’t always solve
+                            the problem if you
+                            don’t have skilled technicians in control.</p>
 
-                            <h2 class="text-center mb-4 text-primary">100% satisfaction Guaranteed</h2>
+                        <h2 class="text-center mb-4 text-primary">100% satisfaction Guaranteed</h2>
 
-                            <?php the_content(); ?>
+                        <?php the_content(); ?>
 
-                        </div>
-                        <?php
+                    </div>
+                    <?php
 
                             endwhile; // End of the loop.
                             ?>
-                    </div>
                 </div>
             </div>
-
-        </section>
+        </div>
 
 
 
