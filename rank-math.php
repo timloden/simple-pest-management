24c1025b7f7cb349898d39d494cb8233b358b861
Tim Loden <tim.m.loden@gmail.com>
2024-01-24 10:44:33 -0800

    added meta description filter

--- rank-math.php
+++ rank-math.php
@@ -30,11 +30,46 @@ add_filter( 'rank_math/frontend/title', function( $title ) {
 
                 $city = get_field('city', $post_id);
                 $title = $city . ' ' . $pest . ' Control | Simple Pest Management';
-
-                error_log($title);
             }
         }
     }
     
     return $title;
+});
+
+add_filter( 'rank_math/frontend/description', function( $description ) {
+    if (is_singular('service-areas')) {
+        global $wp_query;
+        $post_id = get_the_ID();
+        $query_vars = $wp_query->query_vars;
+        
+        $pests = array(
+            'wasps',
+            'rodents',
+            'bed-bugs',
+            'cockroaches',
+            'silverfish',
+            'spiders',
+            'ants',
+            'fleas-ticks-mites',
+            'carpet-beetles',
+            'centipedes',
+            'mosquitos',
+            'earwigs',
+        );
+
+        foreach ($pests as $pest) {
+            if (array_key_exists($pest, $query_vars)) {
+
+                if (str_ends_with($pest, 's')) {
+                    $pest = rtrim($pest, "s");
+                }
+
+                $city = get_field('city', $post_id);
+                $description = '100% Satisfaction Guaranteed - ' . ucfirst($city) . ' ' . ucfirst($pest) . ' Control';
+            }
+        }
+    }
+
+	return $description;
 });
