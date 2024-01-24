82f205160a417d6619f6862d97c3c9d5be950805
Tim Loden <tim.m.loden@gmail.com>
2024-01-24 10:27:15 -0800

    fixed pest page title to show pest and city

---
+++ rank-math.php
@@ -0,0 +1,40 @@
+<?php 
+
+add_filter( 'rank_math/frontend/title', function( $title ) {
+    if (is_singular('service-areas')) {
+        global $wp_query;
+        $post_id = get_the_ID();
+        $query_vars = $wp_query->query_vars;
+        
+        $pests = array(
+            'wasps',
+            'rodents',
+            'bed-bugs',
+            'cockroaches',
+            'silverfish',
+            'spiders',
+            'ants',
+            'fleas-ticks-mites',
+            'carpet-beetles',
+            'centipedes',
+            'mosquitos',
+            'earwigs',
+        );
+
+        foreach ($pests as $pest) {
+            if (array_key_exists($pest, $query_vars)) {
+
+                if (str_ends_with($pest, 's')) {
+                    $pest = rtrim($pest, "s");
+                }
+
+                $city = get_field('city', $post_id);
+                $title = $city . ' ' . $pest . ' Control | Simple Pest Management';
+
+                error_log($title);
+            }
+        }
+    }
+    
+    return $title;
+});
