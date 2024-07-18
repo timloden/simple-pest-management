3c6aac64241c036e3b1b56e6c6dc9951d5c69139
Tim Loden <tim.m.loden@gmail.com>
2024-07-18 15:21:59 -0700

    added career page styling

--- page-careers.php
+++ page-careers.php
@@ -23,9 +23,9 @@ get_header();
 
                 <?php if (have_rows('job_listings')) : ?>
 
-                    <?php while (have_rows('job_listings')) : the_row(); ?>
+                <?php while (have_rows('job_listings')) : the_row(); ?>
 
-                        <?php
+                <?php
                         $job_title = get_sub_field('job_title');
                         $job_short_description = get_sub_field('job_short_description');
                         $job_description = get_sub_field('job_description');
@@ -33,49 +33,55 @@ get_header();
                         $row_id
                         ?>
 
-                        <div class="row mb-3 pb-3 border-bottom">
-                            <div class="col-12">
-                                <h2 class="text-primary font-weight-bold"><?php echo $job_title; ?></h2>
-                                <p><?php echo $job_short_description; ?></p>
-                                <div class="d-flex flex-wrap">
-                                    <div class="col-12 col-lg text-center text-lg-left mb-3 mb-lg-0">
-                                        <a data-target=".job-<?php echo get_row_index(); ?>" class="btn btn-outline-primary font-weight-bold" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="description-<?php echo get_row_index(); ?>">Read full
-                                            description</a>
-                                    </div>
-                                    <div class="col-12 col-lg text-center text-lg-right">
-                                        <a href="#apply-<?php echo get_row_index(); ?>" class="btn btn-orange font-weight-bold" data-toggle="collapse" aria-expanded="false" aria-controls="apply-<?php echo get_row_index(); ?>">Apply for the
-                                            <?php echo $job_title; ?>
-                                            position</a>
-                                    </div>
-                                </div>
+                <div class="row job-listing mb-5 pb-5 border-bottom">
+                    <div class="col-12">
+                        <h2 class="text-primary font-weight-bold"><?php echo $job_title; ?></h2>
+                        <p><?php echo $job_short_description; ?></p>
+                        <div class="d-flex flex-wrap">
+                            <div class="col-12 col-lg text-center text-lg-left mb-3 mb-lg-0">
+                                <a data-target=".job-<?php echo get_row_index(); ?>"
+                                    class="btn btn-outline-primary font-weight-bold" data-toggle="collapse"
+                                    role="button" aria-expanded="false"
+                                    aria-controls="description-<?php echo get_row_index(); ?>">Read full
+                                    description</a>
                             </div>
-
-                            <div class="mt-4 col-12 collapse job-<?php echo get_row_index(); ?>" id="description-<?php echo get_row_index(); ?>">
-                                <h3 class="h5">Full <?php echo $job_title; ?> description</h3>
-                                <?php echo $job_description; ?>
+                            <div class="col-12 col-lg text-center text-lg-right">
+                                <a href="#apply-<?php echo get_row_index(); ?>" class="btn btn-orange font-weight-bold"
+                                    data-toggle="collapse" aria-expanded="false"
+                                    aria-controls="apply-<?php echo get_row_index(); ?>">Apply for the
+                                    <?php echo $job_title; ?>
+                                    position</a>
                             </div>
+                        </div>
+                    </div>
 
-                            <div class="mt-4 col-12 collapse job-<?php echo get_row_index(); ?>" id="apply-<?php echo get_row_index(); ?>">
-                                <div class="bg-light p-3 border rounded">
-                                    <h3 class="h5">Submit your resume</h3>
-                                    <?php
+                    <div class="mt-4 col-12 collapse job-<?php echo get_row_index(); ?>"
+                        id="description-<?php echo get_row_index(); ?>">
+                        <h3 class="h5">Full <?php echo $job_title; ?> description</h3>
+                        <?php echo $job_description; ?>
+                    </div>
+
+                    <div class="mt-4 col-12 collapse job-<?php echo get_row_index(); ?>"
+                        id="apply-<?php echo get_row_index(); ?>">
+                        <div class="bg-light p-3 border rounded">
+                            <h3 class="h5">Submit your resume</h3>
+                            <?php
                                     if ($job_form_id) {
                                         gravity_form($job_form_id, false, false, false, '', true, 12);
                                     }
                                     ?>
-                                </div>
-
-                            </div>
-
                         </div>
+
+                    </div>
+
+                </div>
+
+                <?php endwhile; ?>
+
+                <?php endif; ?>
             </div>
 
-        <?php endwhile; ?>
-
-    <?php endif; ?>
         </div>
-
-    </div>
     </div>
 </section>
 <?php
