6145bd7bbfbc3d067482fb1dcd64598f790becdc
Tim Loden <tim.m.loden@gmail.com>
2024-09-17 16:21:26 -0700

    added versions to css and js

--- inc/scripts-and-styles.php
+++ inc/scripts-and-styles.php
@@ -3,13 +3,13 @@
 /**
  * Enqueue scripts and styles.
  */
-function scripts_and_styles() {
-	wp_enqueue_style( 'base-style', get_stylesheet_uri() );
+function scripts_and_styles()
+{
+	wp_enqueue_style('base-style', get_stylesheet_uri(), [],  wp_get_theme()->get('Version'));
 
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'vendor-js', get_template_directory_uri() . '/assets/js/vendor.min.js', array(), '', true );
-	wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/assets/js/custom.min.js', array(), '', true );
-
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('vendor-js', get_template_directory_uri() . '/assets/js/vendor.min.js', array(), wp_get_theme()->get('Version'), true);
+	wp_enqueue_script('custom-js', get_template_directory_uri() . '/assets/js/custom.min.js', array(), wp_get_theme()->get('Version'), true);
 }
 
-add_action( 'wp_enqueue_scripts', 'scripts_and_styles' );
+add_action('wp_enqueue_scripts', 'scripts_and_styles');
